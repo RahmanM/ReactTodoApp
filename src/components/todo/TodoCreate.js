5df8687fca5e67cd0c6f4115fcd8d3b7b436c9c7
Rahman Mahmoodi <rahman.mahmoodi@gamil.com>
2019-12-04 17:23:37 +1100

    New Todo - title change

--- src/components/todo/TodoCreate.js
+++ src/components/todo/TodoCreate.js
@@ -61,7 +61,7 @@ class TodoCreate extends Component {
 
       <div className="card left-a">
         <div className="card-header">
-          <h5>Create A Todo</h5>
+          <h5>Create New Todo</h5>
         </div>
         <div className="pad-right-50 pad-top-20 card-body left-a">
           <form action="post" method="post" onSubmit={this.addTodos}>
